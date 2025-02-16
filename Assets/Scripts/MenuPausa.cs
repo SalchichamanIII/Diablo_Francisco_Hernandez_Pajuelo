2dbb7b5b0b077456a04280663c249b75e91d0176
SalchichamanIII <163415869+SalchichamanIII@users.noreply.github.com>
2025-02-17 00:39:19 +0100

    160225

---
+++ Assets/Scripts/MenuPausa.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+    [SerializeField] private GameObject menuPausaUI; // Panel del menú de pausa
+    [SerializeField] private AudioSource musicaFondo;
+
+    private bool estaPausado = false;
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (estaPausado)
+            {
+                Reanudar();
+            }
+            else
+            {
+                Pausar();
+            }
+        }
+    }
+
+    public void Pausar()
+    {
+        menuPausaUI.SetActive(true);
+        Time.timeScale = 0f;
+        estaPausado = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        if (musicaFondo != null)
+        {
+            musicaFondo.Pause();
+        }
+    }
+
+    public void Reanudar()
+    {
+        menuPausaUI.SetActive(false);
+        Time.timeScale = 1f;
+        estaPausado = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+        if (musicaFondo != null)
+        {
+            musicaFondo.UnPause();
+        }
+    }
+
+    public void Salir()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(1);
+    }
+}
